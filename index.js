366cdab9e6766e295846f6174cf4ef0c7ab80f7d
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-25 03:57:26 +0000

    commented out manual test code in index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const { fetchMyIP } = require('./iss');
 // const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -20,10 +20,10 @@ const { fetchISSFlyOverTimes } = require('./iss');
 // });
 
 
-fetchISSFlyOverTimes({latitude: '49.27670', longitude: '-123.130000'}, (error, data) => {
-  if (error) {
-    console.log('It didn\'t work! ', error);
-    return;
-  }
-  console.log('It worked! Returned coords: ', data);
-});
+// fetchISSFlyOverTimes({latitude: '49.27670', longitude: '-123.130000'}, (error, data) => {
+//   if (error) {
+//     console.log('It didn\'t work! ', error);
+//     return;
+//   }
+//   console.log('It worked! Returned coords: ', data);
+// });
